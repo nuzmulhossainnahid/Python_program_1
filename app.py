47108784a1c1567780d1694b618d12e22c20e172
nuzmulhossainnahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-04-26 09:56:59 +0000

    2nd Program

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
-# 4A small progrm
+# # 4A small progrm
+#
+# Name = input('What is Your Name ? ')
+# print('Welcome ....'+Name)
+# Age = int(input('How old are you? '))
+# Answer = str(2021 - Age)
+# print('You are born in '+Answer)
 
-Name = input('What is Your Name ? ')
-print('Welcome ....'+Name)
-Age = int(input('How old are you? '))
-Answer = str(2021 - Age)
-print('You are born in '+Answer)
 
+# 6practice-1
+
+# A = input('Enter A Value: ')
+# B = input('Enter B Value: ')
+# temp = A
+# A = B
+# B = temp
+# print('A = '+A)
+# print('B = '+B)
+
+
+# 6practice-2
+# import math
+# A = int(input('Enter A value : '))
+# B = int(input('Enter B value : '))
+# C = int(input('Enter C value : '))
+#
+# D = math.sqrt(B*B-4*A*C)
+# X1 = (-B+D)/(2*A)
+# X2 = (-B-D)/(2*A)
+# print(X1)
+# print(X2)
+
+
+# 6practice-3
+x = input('Enter cal Value: ')
+c = float(x)
+
+f = (9/5)*c+32
+print(f)
