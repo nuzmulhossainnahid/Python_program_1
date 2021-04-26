e45fffb8d6d9b4f0cf8be26141e349073d2af899
nuzmulhossainnahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-04-26 14:02:45 +0000

    Done

--- 4.Function.py
+++ 4.Function.py
@@ -31,21 +31,21 @@
 # fibonacci sequence
 # 0 1 1 2 3 5 8 13 ....
 #
-# def fib(n):
-#     if n == 0:
-#         return 0
-#     else:
-#         x = 0
-#         y = 1
-#         print(x)
-#         print(y)
-#
-#         for i in range(1,n):
-#             z = x+y
-#             x = y
-#             y = z
-#             print(z)
-#
-#
-# fib(int(input('Enter Number: ')))
+def fib(n):
+    if n == 0:
+        return 0
+    else:
+        x = 0
+        y = 1
+        print(x)
+        print(y)
+
+        for i in range(1,n):
+            z = x+y
+            x = y
+            y = z
+            print(z)
+
+
+fib(int(input('Enter Number: ')))
 
