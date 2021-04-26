bbbd523d5c845860e94bb14a1c058fcb973a0196
nuzmulhossainnahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-04-26 09:28:33 +0000

    1st Program

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('Nahid')
+# 4A small progrm
+
+Name = input('What is Your Name ? ')
+print('Welcome ....'+Name)
+Age = int(input('How old are you? '))
+Answer = str(2021 - Age)
+print('You are born in '+Answer)
+
